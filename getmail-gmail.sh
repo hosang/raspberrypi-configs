9224bc06aa67ef92c6a43a064a8381b42a68d8b9
Jan Hosang <jan.hosang@gmail.com>
2015-06-18 07:58:42 +0200

    locking for getmail

--- getmail-gmail.sh
+++ getmail-gmail.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+LOCKFILE="/tmp/getmail-gmail.lock"
+# try once to get the lock else exit
+lockfile -r 0 "$LOCKFILE" || exit 0
+trap 'rm -f "$LOCKFILE"; exit $?' INT TERM EXIT
+
 # Note: -q means fetch quietly so that this program is silent
 /usr/bin/getmail -q -r /home/pi/.getmail/getmail.gmail
